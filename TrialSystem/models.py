070688fff06d615815211292267b4d975e985ebd
Equinox <johnmarkflameno@gmail.com>
2024-11-04 19:23:32 +0800

    Config admin

--- TrialSystem/models.py
+++ TrialSystem/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Customer(models.Model):
+    CustomerName = models.CharField(max_length=100)
+    CustomerNumber = models.IntegerField()
+    CustomerEmail = models.EmailField()
