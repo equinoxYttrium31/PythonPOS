14fb8f0fcb17f228328b32e2cbf563ce05883ed1
Equinox <johnmarkflameno@gmail.com>
2024-11-04 20:33:10 +0800

    Config creation of accs and list

--- TrialSystem/views.py
+++ TrialSystem/views.py
@@ -1,7 +1,9 @@
+from django.urls import reverse
 from tempfile import template
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from . import models
 
 # Create your views here.
 def tryWeb(request):
@@ -14,3 +16,24 @@ def MainPage(request):
         'body': 'This is the body of the main page.',
     }
     return render(request, template, context)
+
+def CreateCustomer(request):
+    template = 'Create_Customer_Account.html'
+    return render(request, template)
+
+def CustomerAccount(request):
+    customers = models.Customer.objects.all()
+    template = 'Customer_List.html'
+    context = {
+        'customers': customers,
+    }
+    return render(request, template, context)
+
+def SaveUser(request):
+    customer = models.Customer()
+    customer.CustomerNumber = request.GET['CustomerNumber']
+    customer.CustomerName = request.GET['CustomerName']
+    customer.CustomerEmail = request.GET['CustomerEmail']
+    customer.save()
+
+    return HttpResponseRedirect(reverse('CustomerAccount'))
--- practice/urls.py
+++ practice/urls.py
@@ -23,4 +23,7 @@ urlpatterns = [
     path('tryWeb', views.tryWeb, name='tryWeb'),
 
     path('MainPage', views.MainPage, name='MainPage'),
+    path('CreateCustomer', views.CreateCustomer, name='CreateCustomer'),
+    path('CustomerAccount', views.CustomerAccount, name='CustomerAccount'),
+    path('SaveUser', views.SaveUser, name='SaveUser'),
 ]
